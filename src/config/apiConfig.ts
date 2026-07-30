b1e880201bcbfa39606027a232baf769a8db8539
noahs <noah.kithcart@nov.com>
2026-07-30 10:24:04 -0400

    fix base api endpoint

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,6 +1,5 @@
 // dotenv.config({ path: `${path.dirname()}/../.env`, override: true });
 console.log('printing process:\n', import.meta);
 
-const API_BASE_URL =
-  import.meta.env.VITE_REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = `${import.meta.env.VITE_REACT_APP_API_URL || 'http://localhost:5000'}/api`;
 export default API_BASE_URL;
